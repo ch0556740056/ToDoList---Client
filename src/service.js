841b0541c45537f8da929886e06a82131385aff5
ch0556740056 <ch0556740056@gmail.com>
2023-04-17 19:26:12 +0300

    my commit message

---
+++ src/service.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+axios.defaults.baseURL='http://localhost:5057';
+
+
+export default {
+  getTasks: async () => {
+     const result=await axios.get(`/items`)
+     return result.data
+  },
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    const result = await axios.post(`/items`,{Name:name,IsComplete:false})   
+    return result.data;
+  },
+  setCompleted: async(id,isComplete)=>{
+    console.log('setCompleted', {id, isComplete})
+    const result = await axios.put(`/items/${id}`,{Name:"",isComplete:true},id)    
+    return result.data;
+  },
+  deleteTask:async(id)=>{ 
+    console.log("deleteTask");
+      await axios.delete(`/items/${id}`)
+  }};
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+  console.log(error);  
+})
+
+
